3debb0e0973a4a64a07dc743b4a5878632eb5315
BareonZahar <zatst2000@yandex.ru>
2023-09-25 21:52:36 +0300

    admin

--- catalog/models.py
+++ catalog/models.py
@@ -2,40 +2,40 @@ from django.db import models
 
 # Create your models here.
 class Genre(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, verbose_name='Жанр')
 
     def __str__(self):
         return self.name
 
 
 class Director(models.Model):
-    fname = models.CharField(max_length=20)
-    lname = models.CharField(max_length=20)
+    fname = models.CharField(max_length=20, verbose_name='Имя')
+    lname = models.CharField(max_length=20, verbose_name='Фамилия')
 
     def __str__(self):
-        return self.lname, self.fname
+        return f'{self.lname},{self.fname}'
 
 
 class Actor(models.Model):
-    fname = models.CharField(max_length=20)
-    lname = models.CharField(max_length=20)
-    born = models.DateField()
-    country = models.CharField(max_length=20)
+    fname = models.CharField(max_length=20, verbose_name='Имя')
+    lname = models.CharField(max_length=20, verbose_name='Фамилия')
+    born = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    country = models.CharField(max_length=20, blank=True, null=True, verbose_name='Страна')
 
     def __str__(self):
-        return self.fname
+        return self.lname
 
 
 class Status(models.Model):
     VIBOR = (('бесплатно','бесплатно'),('базовая','базовая'),('супер','супер'))
-    name = models.CharField(max_length=60, choices=VIBOR)
+    name = models.CharField(max_length=60, choices=VIBOR, verbose_name='Подписка')
 
     def __str__(self):
         return self.name
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, verbose_name='Страна')
 
     def __str__(self):
         return self.name
@@ -43,27 +43,33 @@ class Country(models.Model):
 
 class AgeRate(models.Model):
     choise = (('G','G'),('PG','PG'),('PG-13','PG-13'),('R','R'),('NC-17','NC-17'))
-    rate = models.CharField(max_length=20,choices=choise)
+    rate = models.CharField(max_length=20,choices=choise, verbose_name='Рейтинг')
 
     def __str__(self):
         return self.rate
 
 
 class Kino(models.Model):
-    title = models.CharField(max_length=30)
-    genre = models.ForeignKey(Genre, on_delete=models.SET_DEFAULT, default=1)
-    rating = models.FloatField()
+    title = models.CharField(max_length=30, verbose_name='Название')
+    genre = models.ForeignKey(Genre, on_delete=models.SET_DEFAULT, default=1, verbose_name='Жанр')
+    rating = models.FloatField(verbose_name='Оценка')
     country = models.ForeignKey(Country, on_delete=models.SET_NULL,null=True)
-    director = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=500)
-    year = models.IntegerField()
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=500, verbose_name='Описание')
+    year = models.IntegerField(verbose_name='Год')
     ager = models.ForeignKey(AgeRate, on_delete=models.SET_NULL, null=True)
-    actor = models.ManyToManyField(Actor)
-    status = models.ForeignKey(Status,on_delete=models.SET_NULL, default=1)
+    actor = models.ManyToManyField(Actor, verbose_name='Актер')
+    status = models.ForeignKey(Status,on_delete=models.SET_DEFAULT, default=1)
 
     def __str__(self):
         return self.title
 
+    def display_actors(self):
+        res = ''
+        for a in self.actor.all():
+            res += a.lname+' '
+        return res
+    display_actors.short_description = 'Актеры'
 
 
 
