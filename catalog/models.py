dd1d5a186a10bc8fa2d8eaff3657593d87db95eb
BareonZahar <zatst2000@yandex.ru>
2023-09-26 21:57:55 +0300

    vetclin model2

--- catalog/models.py
+++ catalog/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Doctors(models.Model):
-    surgeon = models.CharField(max_length=30)
-    dentist  = models.CharField(max_length=30)
-    therapist = models.CharField(max_length=30)
-    dermatologist = models.CharField(max_length=30)
 
+
+class Doctors(models.Model):
+    surname = models.CharField(max_length=20)
+    name = models.CharField(max_length=15)
+    profesh = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.surgeon,self.dentist,self.dermatologist
+        return self.surname, self.name, self.profesh
+
+
+class Status(models.Model):
+    VIBOR = (('хирург','хирург'),('стоматолог','стоматолог'),
+             ('терапевт','терапевт'),('дерматолог','дерматолог'))
+    prof = models.CharField(max_length=20, choices=VIBOR)
+
+    def __str__(self):
+        return self.prof
 
 
 class Services(models.Model):
@@ -21,10 +30,10 @@ class Services(models.Model):
     delivery_reception = models.TextField(max_length=500)
     stomat_ment = models.TextField(max_length=500)
 
-
-
     def __str__(self):
-        return self.first_inspection
+        return f'{self.first_inspection},{self.laboratory_tests},{self.cupping},' \
+               f'{self.haircuts},{self.nail_trimming},{self.delivery_reception},{self.stomat_ment}'
+
 
 class Prices(models.Model):
     first_inspection = models.IntegerField()
@@ -35,10 +44,9 @@ class Prices(models.Model):
     delivery_reception = models.IntegerField()
     stomat_ment = models.IntegerField()
 
-
-
     def __str__(self):
-        return self.first_inspection
+        return f'{self.first_inspection},{self.laboratory_tests},{self.cupping},' \
+               f'{self.haircuts},{self.nail_trimming},{self.delivery_reception},{self.stomat_ment}'
 
 
 class Animal_card(models.Model):
@@ -47,8 +55,6 @@ class Animal_card(models.Model):
     age = models.IntegerField()
     height = models.IntegerField()
     weight = models.IntegerField()
-
-
 
     def __str__(self):
         return self.nickname
@@ -63,36 +69,37 @@ class Owner_card(models.Model):
     breed = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
 
-
-
     def __str__(self):
         return self.surname
 
 
-class Status(models.Model):
-    Vibor = (('бесплатно','бесплатно'),('стандартная','стандартная'),('премиум','премиум'))
-    name = models.CharField(max_length=60)
-
-
+class Breed(models.Model):
+    breed = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.name
+        return self.breed
+
+
+class Data(models.Model):
+    data = models.DateField()
+
+    def __str__(self):
+        return self.data
 
 
 class Vet_clinic(models.Model):
-    title = models.CharField(max_length=30)
     doctor = models.ManyToManyField(Doctors)
     services = models.ForeignKey(Services, on_delete=models.SET_NULL,null=True)
+    prices = models.ForeignKey(Prices, on_delete=models.SET_NULL,null=True)
     owner_card = models.ForeignKey(Owner_card, on_delete=models.CASCADE)
     animal_card = models.ForeignKey(Animal_card, on_delete=models.CASCADE)
-    price = models.DecimalField(decimal_places=2,max_digits=9)
-    contacts = models.IntegerField()
-    subscription = models.ForeignKey(Status,on_delete=models.SET_NULL,default=1)
-
-
+    servis_price = models.DecimalField(decimal_places=3,max_digits=9)
+    breeds = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    data = models.ForeignKey(Data, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.SET_DEFAULT, default=1)
 
     def __str__(self):
-        return self.title
+        return self.doctor
 
 
 
