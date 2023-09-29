8ece15b58459a0d15d5591a77d4f5ba4912f07fd
BareonZahar <zatst2000@yandex.ru>
2023-09-30 00:59:06 +0300

    vet mod-15

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(req):
-    return render(req,'index.html')
+    numbreed = Breed.objects.all().count()
+    numdoctor = Doctors.objects.all().count()
+    numservis = Vet_clinic.objects.filter(services__vet_clinic=1).count()
+    data = {'k1':numbreed,'k2':numdoctor,'k3':numservis}
+    return render(req,'index.html',context=data)
